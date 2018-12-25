9a813d6bd177538bc7e4cab4dea11d58f70e695a
AnuragMakol <anurag.makol@gmail.com>
2018-12-25 17:16:01 +0530

    Few Changes

--- resources/db.js
+++ resources/db.js
@@ -1,16 +1,13 @@
 var mongoose = require("mongoose");
 var beautifyUnique = require("mongoose-beautiful-unique-validation");
-mongoose.connect(process.env.MONGO_URL,{
-  useNewUrlParser: true
-});
+mongoose.connect(process.env.MONGO_URL,{ useNewUrlParser: true });
 
-// Include Schemas
-var users = require('./db/users')
+var users = require('./db/users');
 
-// Integrate Beautify Unique Plugin to required collections
 users.plugin(beautifyUnique);
 
-// Export Collections
-module.exports = {
+var database = {
   "users": mongoose.model("users", users),
-};
+}
+
+module.exports = database;
