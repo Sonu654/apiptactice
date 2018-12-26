900fbcaa729516420269dfaecd8475bde8333acd
AnuragMakol <anurag.makol@gmail.com>
2018-12-26 11:08:32 +0530

    Error Handler Added

--- app.js
+++ app.js
@@ -9,12 +9,8 @@ var dotenv = require('dotenv').config()
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'twig');
-
 // Express Settings
-app.use(logger('dev'));
+app.use(logger('combined'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -38,13 +34,10 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500).send({
+    status: err.status,
+    message: err.message
+  });
 });
 
 module.exports = app;
